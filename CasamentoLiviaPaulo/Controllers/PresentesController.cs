241cecaff0f975e7294c612a16e102abbe73f229
Paulo Victor <paulinho_vtr@live.com>
2021-01-15 02:03:09 -0300

    Criado pagina de presentes e separado topo e rodapé para a Layout

---
+++ CasamentoLiviaPaulo/Controllers/PresentesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasamentoLiviaPaulo.Controllers
+{
+    public class PresentesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
